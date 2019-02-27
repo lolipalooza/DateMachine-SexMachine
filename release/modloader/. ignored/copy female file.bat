bdea14b7e05c7d25e4486b02cdeef34f72a3b8a9
mumifaglalolanda <mumifag_lalolanda@hotmail.com>
2019-02-27 14:21:38 -0400

    Removing the release folder from the project

--- release/modloader/. ignored/copy female file.bat
+++
@@ -1,7 +0,0 @@
-set file=%~n0
-
-:: C:\Progra~1\7-Zip\7z.exe x "%file%.zip"
-copy "models\%file%.dff" ..\asdf\female.dff
-copy "models\%file%.txd" ..\asdf\female.txd
-
-:: pause >nul
--- release/modloader/. ignored/copy male file.bat
+++
@@ -1,8 +0,0 @@
-set filename=%~n0
-set file=%filename:~0,-1%
-
-:: C:\Progra~1\7-Zip\7z.exe x "%file%.zip"
-copy "models\%file%.dff" ..\asdf\male.dff
-copy "models\%file%.txd" ..\asdf\male.txd
-
-:: pause >nul
